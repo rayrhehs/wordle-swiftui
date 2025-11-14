dad4da64ccaf941c5818bb094acb91ff16317d27
Shehryar M <shehryarm1029@gmail.com>
2025-11-13 20:20:33 -0500

    Added basic ui

--- Wordle/ContentView.swift
+++ Wordle/ContentView.swift
@@ -8,16 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let keyRows = ["QWERTYUIOP", "ASDFGHJKL", "ZXCVBNM"]
+    @State private var answer:[String] = []
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ForEach(0..<5) { row in
+                HStack {
+                    ForEach(0..<5) { column in
+                        ZStack {
+                            Rectangle()
+                                .stroke(Color.black, lineWidth: 4)
+                                .frame(width: 65, height: 65)
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity, alignment: .center)   // center each row
+            }
+            VStack {
+                // make HStack for each keyRow
+                ForEach(0..<keyRows.count, id: \.self) { rowIndex in
+                    let rowString = Array(keyRows[rowIndex])
+                    HStack {
+                        ForEach(0..<rowString.count, id: \.self) { letterIndex in
+                            Button(action: {
+                                if answer.count < 5 {
+                                    answer.append(String(rowString[letterIndex]))
+                                    print(answer)
+                                }
+                            }) {
+                                Text(String(rowString[letterIndex]))
+                                    .frame(width: 30, height: 75)
+                                    .background(Color.green)
+                                    .cornerRadius(6)
+                            }
+                        }
+                    }
+                }
+                HStack {
+                    Button(action: {
+                        if !answer.isEmpty {
+                            answer.removeLast()
+                            print(answer)
+                        }
+                    }) {
+                        Text("DELETE")
+                    }
+                }
+            }
         }
+        .frame(maxWidth: .infinity, alignment: .center)           // center the whole grid
         .padding()
     }
 }
+
 
 #Preview {
     ContentView()
